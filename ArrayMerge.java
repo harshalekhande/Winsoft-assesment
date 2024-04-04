bf182ffa15fb3d2d95c267fa2fcb0e3ee2e4dca8
Harshal EKhande <86643851+harshalekhande@users.noreply.github.com>
2024-04-04 13:58:28 +0530

    Add files via upload

---
+++ ArrayMerge.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class ArrayMerge {
+
+    private static void merge(int[] X, int[] Y, int m, int n)
+    {
+        int k = m + n + 1;
+        while (m >= 0 && n >= 0)
+        {
+            if (X[m] > Y[n]) {
+                X[k--] = X[m--];
+            }
+            else {
+                X[k--] = Y[n--];
+            }
+        }
+
+        while (n >= 0) {
+            X[k--] = Y[n--];
+        }
+
+        Arrays.fill(Y, 0);
+    }
+
+    public static void arrange(int[] X, int[] Y)
+    {
+        if (X.length == 0) {
+            return;
+        }
+
+        int k = 0;
+        for (int value: X)
+        {
+            if (value != 0) {
+                X[k++] = value;
+            }
+        }
+
+        merge(X, Y, k - 1, Y.length - 1);
+    }
+
+    public static void main (String[] args)
+    {
+        int[] X = { 0, 4, 0, 5, 0, 6, 7, 0, 0};
+        int[] Y = { 1, 3, 8, 11, 13 };
+        arrange(X, Y);
+        System.out.println(Arrays.toString(X));
+    }
+}
